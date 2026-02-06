3efcbd0ea0264615e0c33849d726eeb3dab9c0f0
reemfad <reemfad51@gmail.com>
2026-02-06 08:06:58 +0200

    activating production environment

--- src/app.py
+++ src/app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 # Initialize DagsHub
 # gpa_predictorP
-# dagshub.init(repo_owner='reemfad51', 
-#              repo_name='student-gpa-prediction', 
-#              mlflow=True)
-mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
-print("here is where i performed a change")
+dagshub.init(repo_owner='reemfad51', 
+             repo_name='student-gpa-prediction', 
+             mlflow=True)
+# mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
+
 # Global variables
 model = None
 preprocessor = None
