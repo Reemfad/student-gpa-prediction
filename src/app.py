33539422a0ee6a4f05a043f7cdead505f106d5dd
reemfad <reemfad51@gmail.com>
2026-02-06 15:29:07 +0200

    fix ml container start

--- src/app.py
+++ src/app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 # Initialize DagsHub
 # gpa_predictorP
-# dagshub.init(repo_owner='reemfad51', 
-#              repo_name='student-gpa-prediction', 
-#              mlflow=True)
-mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
+dagshub.init(repo_owner='reemfad51', 
+             repo_name='student-gpa-prediction', 
+             mlflow=True)
+# mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
 print("here is where i performed a change")
 # Global variables
 model = None
